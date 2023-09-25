405073d347dc77894b557597b0af50edbf35ee69
U-BSD\453364 <453364@SHS-85W2NW3.beaverton.k12.or.us>
2023-09-25 09:20:51 -0700

    first commit

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -17,18 +17,21 @@ const int playertwo = 0;
 //all the methods used in the porjects and every var that was passed inside them
 int printBoard(int board[3][3], int row);
 int setBoard(int board[3][3]);
-int playerMove(int board[3][3], int X_MOVE, int O_MOVE, int rowmove, int columnmove, int &move, int playertwo, bool &win, int &playercount);
+int playerMove(int board[3][3], int X_MOVE, int O_MOVE, int rowmove, int columnmove, int &move, int playertwo, bool &win, int &playercount, bool &valid);
 bool checkWin (int board[3][3], int playercount);
-
+bool isValid (int columnmove, int rowmove, int board[3][3]);
 
 int main () { //this is the main part of the code, where all the other methods are called from, this is also where the user can play again
   char repeat = ' ';
   bool playAgain = true;
   int playercount = 0;
+  bool valid = true;
   while (playAgain) {
   int move = 0;
   int rowmove = 0;
   int columnmove = 0;
+  int xwincounter = 0;
+  int owincounter = 0;
   bool win = true;//the win condtion to see if the game is over
   int board[3][3];  //the array used for thr 3x3 board
   int row = 3;
@@ -36,22 +39,28 @@ int main () { //this is the main part of the code, where all the other methods a
   printBoard(board, row); //printing the board after all of the values are set
   move = playerone; //setting the move to player one or X
   while (move == playerone) { //while the move is x move, they can move and reprint the board
-    playerMove(board, X_MOVE, O_MOVE, rowmove, columnmove, move, playertwo, win, playercount);
+    playerMove(board, X_MOVE, O_MOVE, rowmove, columnmove, move, playertwo, win, playercount, valid);
     printBoard(board, row);
     if (win == false) { //if no one has won yet then you can continue
-      playerMove(board, X_MOVE, O_MOVE, rowmove, columnmove, move, playertwo, win, playercount);
+      playerMove(board, X_MOVE, O_MOVE, rowmove, columnmove, move, playertwo, win, playercount, valid);
 	printBoard(board, row);
     }
     else if (win == true) { //if won, who has won, and do they want to play again?
-
+      
       if (playercount == 9) {
 	cout << "tie" << endl;
       }
-      else if (move == O_MOVE) {
-	cout << "O Wins" << endl;
+      else if (move == playertwo) {
+	owincounter++;
+	cout << "O Wins : ";
+	cout << owincounter << endl;
+	
       }
-      else if (move == X_MOVE) {
-	cout << "X Wins" << endl;
+      else if (move == playerone) {
+	xwincounter++;
+	cout << "X Wins: ";
+	cout << xwincounter << endl;;
+  
       }
     }
 
@@ -121,9 +130,9 @@ int setBoard (int board [3][3]) { //setting the board by default to all empty va
     return 0;
   }
 
-int playerMove (int board[3][3], int X_MOVE, int O_MOVE, int rowmove, int columnmove, int &move, int playertwo, bool &win, int &playercount) { //this method allows the player to move
- 
-
+int playerMove (int board[3][3], int X_MOVE, int O_MOVE, int rowmove, int columnmove, int &move, int playertwo, bool &win, int &playercount, bool &valid) { //this method allows the player to move
+  valid = true;
+  cout << move << endl;
   if (move == playerone) { //start by entering which column and row you want to move
   cin >> rowmove;
   cin >> columnmove;
@@ -134,47 +143,89 @@ int playerMove (int board[3][3], int X_MOVE, int O_MOVE, int rowmove, int column
   columnmove = columnmove - 1;
 
 
-  if (board[columnmove][rowmove] != O_MOVE) { //if you're move is valid, you can move
-    board[columnmove][rowmove] = X_MOVE;
-    playercount++; //playercount, used to check ties, increases
-    cout << playercount << endl;
-  }
-  else {
-    cout << "invalid move" << endl; //invalid move printed if move isn't possible
-  }
+  while (valid) {
+    cout << "enter" << endl;
+    valid = isValid(columnmove, rowmove, board); //check if it is valid
 
+  if (valid == true) {
+    board[columnmove][rowmove] = X_MOVE;
+    playercount++;
+    valid = false;
+  }
+  else { //if invalid go again
+    cout << "Invalid move, enter again" << endl;
+    printBoard(board, 3);
+    cin >> rowmove;
+    cin >> columnmove;
+
+    rowmove = rowmove - 1;
+    columnmove = columnmove - 1;
+    valid = true; 
+  }
+  
   win = checkWin (board, playercount); //call win condition after every move
 
   if (win == false) {
     move = playertwo;
+    
   }
- }
-  else { //same goes for the O move
+  }
+  }
+
+  else { //same goes for the O mov e
     cin >> rowmove;
   cin >> columnmove;
+
+  valid = true;
 
   rowmove = rowmove - 1;
   columnmove = columnmove - 1;
 
-  if (board[columnmove][rowmove] != X_MOVE) {
+  cout << "enter" << endl;
+
+
+  while (valid) {
+    cout << "enter" << endl;
+  valid = isValid(columnmove, rowmove, board);
+
+  if (valid == true) {
     board[columnmove][rowmove] = O_MOVE;
     playercount++;
-    cout << playercount << endl;
+    valid = false;
   }
   else {
-    cout << "invalid move" << endl;
+    cout << "Invalid move, enter again" << endl;
+    printBoard(board, 3);
+    cin >> rowmove;
+    cin >> columnmove;
+
+    rowmove = rowmove - 1;
+    columnmove = columnmove - 1;
+    valid = true;
   }
 
   win = checkWin (board, playercount);
 
+  }
   if (win == false) {
     move = playerone;
   }
 
   }
-  
+
   return 0;
 }
+
+
+
+  bool isValid (int columnmove, int rowmove, int board [3][3]) {
+
+    if (board[columnmove][rowmove] == EMPTY) {
+      return true;
+    }
+    
+    return false;
+  }
 
 bool checkWin (int board[3][3], int playercount) { //this function checks if a win has occured (vertical, horizontal, or diagonal, or tie)
   //x win conditions
@@ -252,3 +303,10 @@ bool checkWin (int board[3][3], int playercount) { //this function checks if a w
 //function to check if move valid
 //make the move
 //Exit, switch moves
+
+
+
+//NEW NOTES
+//is_valid_move;
+//place_player;
+ 
